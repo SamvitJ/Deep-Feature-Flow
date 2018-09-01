b88d51d0b4682af6c3b0a52c28e2e317ae70111d
SamvitJ <samvit.jain@gmail.com>
2018-09-01 02:32:48 +0000

    Switch to trained demo model.

--- dff_deeplab/demo.py
+++ dff_deeplab/demo.py
@@ -136,8 +136,8 @@ def main():
     pprint.pprint(config)
     config.symbol = 'resnet_v1_101_flownet_deeplab'
     model1 = '/../model/rfcn_dff_flownet_vid'
-    # model2 = '/../model/deeplab_dcn_cityscapes'
-    model2 = '/../output/dff_deeplab/cityscapes/resnet_v1_101_flownet_cityscapes_deeplab_end2end_ohem/leftImg8bit_train/' + model_name + '/dff_deeplab_vid'
+    model2 = '/../model/trained/accel-34s-0039'
+    # model2 = '/../output/dff_deeplab/cityscapes/resnet_v1_101_flownet_cityscapes_deeplab_end2end_ohem/leftImg8bit_train/' + model_name + '/dff_deeplab_vid'
     sym_instance = eval(config.symbol + '.' + config.symbol)()
     key_sym = sym_instance.get_key_test_symbol(config)
     cur_sym = sym_instance.get_cur_test_symbol(config)
